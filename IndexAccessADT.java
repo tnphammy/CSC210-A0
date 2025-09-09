9296308f8038770ade232f46a916db3e976c81f2
tnphammy <tnpham@smith.edu>
2025-09-08 20:56:50 -0400

    Rewrote IndexAccess Interface to have get, set, getLength type operations, and move more complex operations that involve a second array and or indices to SequenceADT

--- IndexAccessADT.java
+++ IndexAccessADT.java
@@ -1,75 +1,112 @@
-/** Interface to describe the operations supported directly 
+/**
+ * Interface to describe the operations supported directly
  * by the built-in array.
+ * 
  * Arguments for building an IndexAccess Object:
  * public int length;
  * public T dataType; //int[], char[], etc.
- * */
+ */
 interface IndexAccessADT<T> {
     /**
      * Print a String representation of an array
-     * @param arrayT The array to be represented
      * @return The array as a String
-     * Throws NegativeArraySizeException if length of array is < 0
+     * @throws NegativeArraySizeException if length of array is < 0
      */
-    public String toString(T arrayT);
+    public String toString() throws NegativeArraySizeException;
 
     /**
-     * Compares two arrays
-     * @param array1 The first array
-     * @param array2 The second array
-     * @return 0 if two arrays are equal; 
-     * @return a negative integer if the array1 is less than array2 lexicographically
-     * @return a positive integer if array1 is greater than array2 lexicographically.
+     * Get the first occuring index of a value in an array
+     * @param value The specified value
+     * @return The index of the first occurence of the value
+     * Should return -1 if the value does not exist in the array
      */
-    public int compare(T array1, T array2);
+    public int getIndex(T value);
 
     /**
-     * Checks whether two arrays are the exact same
-     * @param array1 The first array
-     * @param array2 The second array
-     * @return true/false depending on whether the arrays match
+     * Gets the value from a given index of the array
+     * @param index The specified index
+     * @return The value at the given index in the array
+     * @throws ArrayIndexOutOfBoundsException if index is not within the array
      */
-    public boolean equals(T array1, T array2);
-  
-    /**
-     * Fills in an array with a value in a specified range
-     * @param arrayT The array to be modified         
-     * @param start The index of the first position of the range to fill in (Inclusive)
-     * @param end The index of the last position of the range to fill in (Exclusive)
-     * @param value The value used to fill in
-     * Throws IllegalArgumentException if start index < end index
-     * Throws ArrayIndexOutOfBoundsException if either index is not within the array
-     */
-    public void fill(T arrayT, int start, int end, T value);
+    public T getValue(int index) throws ArrayIndexOutOfBoundsException;
 
     /**
-     * Fills in an array with a value 
-     * @param arrayT The array to be modified         
-     * @param value The value used to fill in
+     * Sets the value at the index to a specified value
+     * @param index The index at which to change its value
+     * @param newValue The new value after setting
+     * @throws ArrayIndexOutOfBoundsException if index is not within the array
      */
-    public void fill(T arrayT, T value);
-    
-    /**
-     * Finds the first index of a mismatch between two arrays
-     * @param array1 The first array 
-     * @param array2 The second array
-     * @return The index of the first instance of a mismatch
-     */
-    public int mismatch(T array1, T array2);
+    public void setValue(int index, T newValue) throws ArrayIndexOutOfBoundsException;
 
     /**
-     * Sort an array in ascending order
-     * @param arrayT The array to be sorted
+     * Gets the length of the array
+     * @return The length of the array
      */
-    public void sort(T arrayT);
+    public int getArrayLength();
 
-    /**
-     * Sort a specified range of an array in ascending order
-     * @param arrayT The array to be modified
-     * @param start The index of the first position of the range to fill in (Inclusive)
-     * @param end The index of the last position of the range to fill in (Exclusive)
-     * * Throws IllegalArgumentException if start index < end index
-     * Throws ArrayIndexOutOfBoundsException if either index is not within the array
+    /*
+     * UNIT TESTING
+     * -----------------------------------------------------------
+     * 1. Object Creation
+     * I would make an object with a specific length and data type
+     * For instance,
+     * 
+     * myArray = new int[5] 
+     * 
+     * int[]: The array stores ints
+     * 5: The length of the array is 5
+     * 
+     * -----------------------------------------------------------
+     * 2. Method Calling
+     * 
+     * a) toString()
+     * Call: myArray.toString()
+     * -> Expected return: [0,0,0,0,0] - since there are no set values.
+     * It should default 0 as each index's value, and retain the length of the array as specified (in this case, 5).
+     * 
+     * EXCEPTION:
+     * Sample case:
+     * myArray = int[-1]
+     * -> Expected return: NegativeArraySizeException 
+     * It is impossible to make an array with a negative length.
+     * 
+     * b) setValue(index, value)
+     * Modify Object/Call: 
+     * myArray.setValue(0, 1)
+     * myArray.setValue(1, 2)
+     * myArray.setValue(2, 3)
+     * myArray.setValue(3, 4)
+     * myArray.setValue(4, 5)
+     * This would not return anything, but rather simply modify myArray.
+     * -> If I called myArray.toString(), the expected output would be [1,2,3,7,5]
+     * 
+     * EXCEPTION:
+     * an ArrayIndexOutOfBoundsException should be thrown if index is not within the array
+     * Sample case:
+     * myArray.setValue(10, 7) 
+     * -> Expected return: ArrayIndexOutOfBoundsException
+     * 
+     * c) getValue(index)
+     * Call: myArray.getValue(0)
+     * -> Expected return: 1
+     * 
+     * EXCEPTION:
+     * an ArrayIndexOutOfBoundsException should be thrown if index is not within the array
+     * Sample case:
+     * myArray.getValue(10)
+     * -> Expected return: ArrayIndexOutOfBoundsException
+     * 
+     * d) getIndex(value)
+     * Call: myArray.getIndex(3)
+     * -> Expected return: 2
+     * 
+     * (This isn't an exception, but an expected behavior: if a value does not exist in the array, return -1)
+     * Call: myArray.getIndex(1000)
+     * -> Expected return: -1
+     * 
+     * e) getLength()
+     * Call: myArray.getLength()
+     * -> Expected return: 5 (should be the same as the number between square brackets when declaring)
+     * 
      */
-    public void sort(T arrayT, int start, int end);
 }
---
+++ SequenceADT.java
@@ -0,0 +1,96 @@
+interface SequenceADT<T> {
+    /*
+     * Interface shows the operations one could do with a sequence, a sequence with
+     * an index, and two sequences
+     */
+    /* 1. A Sequence Alone */
+    /**
+     * Print a String representation of an sequence
+     * 
+     * @return The sequence as a String
+     * @throws NegativeArraySizeException if length of sequence is < 0
+     */
+    public String toString() throws NegativeArraySizeException;
+
+    /**
+     * Gets the length of the sequence
+     * 
+     * @return The length of the sequence
+     */
+    public int getArrayLength();
+
+    /* 2. A Sequence with Indeces */
+    /**
+     * Get the first occuring index of a value in an sequence
+     * 
+     * @param value The specified value
+     * @return The index of the first occurence of the value
+     *         Should return -1 if the value does not exist in the sequence
+     */
+    public int getIndex(T value);
+
+    /**
+     * Sets the value at the index to a specified value
+     * 
+     * @param index    The index at which to change its value
+     * @param newValue The new value after setting
+     * @throws ArrayIndexOutOfBoundsException if index is not within the sequence
+     */
+    public void setValue(int index, T newValue) throws ArrayIndexOutOfBoundsException;
+
+    /**
+     * Gets the value from a given index of the sequence
+     * 
+     * @param index The specified index
+     * @return The value at the given index in the sequence
+     * @throws ArrayIndexOutOfBoundsException if index is not within the sequence
+     */
+    public T getValue(int index) throws ArrayIndexOutOfBoundsException;
+
+    /**
+     * Sort a specified range of an sequence in ascending order
+     * 
+     * @param arrayT The sequence to be modified
+     * @param start  The index of the first position of the range to fill in
+     *               (Inclusive)
+     * @param end    The index of the last position of the range to fill in
+     *               (Exclusive)
+     * @throws IllegalArgumentException       if start index < end index
+     * @throws ArrayIndexOutOfBoundsException if either index is not within the
+     *                                        sequence
+     */
+    public void sort(T arrayT, int start, int end);
+
+    /* 3. Two Sequences */
+    /**
+     * Compares two sequences
+     * 
+     * @param array1 The first sequence
+     * @param array2 The second sequence
+     * @return 0 if two arrays are equal;
+     * @return a negative integer if the array1 is less than array2
+     *         lexicographically
+     * @return a positive integer if array1 is greater than array2
+     *         lexicographically.
+     */
+    public int compare(T array1, T array2);
+
+    /**
+     * Checks whether two sequences are the exact same
+     * 
+     * @param array1 The first sequence
+     * @param array2 The second sequence
+     * @return true/false depending on whether the arrays match
+     */
+    public boolean equals(T array1, T array2);
+
+    /**
+     * Finds the first index of a mismatch between two sequences
+     * 
+     * @param array1 The first sequence
+     * @param array2 The second sequence
+     * @return The index of the first instance of a mismatch
+     *         (Provided all sequences had indices)
+     */
+    public int mismatch(T array1, T array2);
+}
